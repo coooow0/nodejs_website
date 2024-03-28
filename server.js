c397b670d0d8929a0df7d9e6af9b39eeb5c10656
coooow0 <yxxng2_@naver.com>
2024-03-29 06:32:08 +0900

    로그인 구현 미친것

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const { MongoClient } = require('mongodb')
+const methodOverride = require('method-override')
+const flash = require('express-flash');
+const bodyParser = require('body-parser');
+const { ObjectId } = require('mongodb');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(methodOverride('_method'))
 
 require('dotenv').config()
 
@@ -15,13 +23,14 @@ const LocalStrategy = require('passport-local')
 
 app.use(passport.initialize())
 app.use(session({
-    secret: process.env.SESSION_PSWD,
+    secret: '암호화에 쓰일 비번',
     resave: false, // 유저가 서버로 요청할 때마다 세션 갱신할 것인지?
     saveUninitialized: false, // 로그인 안 해도 세션 만들것인지?
-    cookie : { maxAge : 60 * 60 * 1000 }
+    cookie: { maxAge: 60 * 60 * 1000 }
 }))
 
 app.use(passport.session())
+app.use(flash());
 
 let db
 const url = process.env.MONGO_DB_PATH
@@ -54,42 +63,43 @@ app.get('/chatting', async (req, res) => {
     res.render('chatting.ejs')
 })
 
-passport.use(new LocalStrategy(async (입력한아이디, 입력한비번, cb) => {
-    let result = await db.collection('user').findOne({ username: 입력한아이디 })
+passport.use(new LocalStrategy(async (username, password, cb) => { // 인자 이름 수정
+    let result = await db.collection('user').findOne({ username: username }); // username 사용
     if (!result) {
-        return cb(null, false, { message: '아이디 DB에 없음' })
+        return cb(null, false, { message: '아이디 DB에 없음' });
     }
-    if (result.password == 입력한비번) {
-        return cb(null, result)
+    if (result.password == password) {
+        return cb(null, result);
     } else {
         return cb(null, false, { message: '비번불일치' });
     }
-}))
+}));
 
 passport.serializeUser((user, done) => {
-    console.log(user)
     process.nextTick(() => {
         done(null, { id: user._id, username: user.username })
     })
 })
 
-passport.deserializeUser((user, done) => {
+passport.deserializeUser(async (user, done) => {
+    let result = await db.collection('user').findOne({ _id: new ObjectId(user.id) })
+    delete result.password
     process.nextTick(() => {
-      return done(null, user)
+        return done(null, result)
     })
-  })
+});
+app.get('/login', async (req, res) => {
+    res.render('login.ejs');
+});
 
-app.get('/login', (req, res)=>{
-    res.render('login.ejs')
-})
+app.post('/login', (req, res, next) => {
+    console.log('사용자가 입력한 아이디:', req.body.username);
+    console.log('사용자가 입력한 비밀번호:', req.body.password);
 
-app.post('/login', async (req, res, next) => {
-    passport.authenticate('local', (error, user, info) => {
-        if (error) return res.status(500).json(error)
-        if (!user) return res.status(401).json(info.message)
-        req.logIn(user, (err) => {
-            if (err) return next(err)
-            res.redirect('/')
-        })
-    })(req, res, next)
-})
+    passport.authenticate('local', {
+        successRedirect: '/', // 로그인 성공 시 홈페이지로 리다이렉션
+        failureRedirect: '/login', // 로그인 실패 시 로그인 페이지로 리다이렉션
+        failureFlash: true // 로그인 실패 시 플래시 메시지 표시
+    })(req, res, next);
+});
+
