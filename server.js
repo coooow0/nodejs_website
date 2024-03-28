cdda5feb199aaa28d6d162ac73ff33e37cad76c7
coooow0 <yxxng2_@naver.com>
2024-03-29 05:35:20 +0900

    css 수정, 로그인 세션 구현

--- server.js
+++ server.js
@@ -15,12 +15,13 @@ const LocalStrategy = require('passport-local')
 
 app.use(passport.initialize())
 app.use(session({
-  secret: process.env.SESSION_PSWD,
-  resave : false, // 유저가 서버로 요청할 때마다 세션 갱신할 것인지?
-  saveUninitialized : false // 로그인 안 해도 세션 만들것인지?
+    secret: process.env.SESSION_PSWD,
+    resave: false, // 유저가 서버로 요청할 때마다 세션 갱신할 것인지?
+    saveUninitialized: false, // 로그인 안 해도 세션 만들것인지?
+    cookie : { maxAge : 60 * 60 * 1000 }
 }))
 
-app.use(passport.session()) 
+app.use(passport.session())
 
 let db
 const url = process.env.MONGO_DB_PATH
@@ -33,33 +34,62 @@ new MongoClient(url).connect().then((client) => {
     })
 }).catch((err) => {
     console.log(err)
-    
+
 })
 
 
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
-    
+
 })
 
-app.get('/name', (req, res) => {
-    res.send('제 이름은 영이에요 :))')
-})
 
-app.get('/hello', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
-
-app.get('/news', ()=>{
-    db.collection('post').insertOne({title:'어쩌고'})
-})
-
-app.get('/list', async(req, res)=>{
+app.get('/list', async (req, res) => {
     let result = await db.collection('post').find().toArray()
     res.send(result[0].title)
 })
 
-app.get('/chatting', async(req, res)=>{
+app.get('/chatting', async (req, res) => {
     res.render('chatting.ejs')
+})
+
+passport.use(new LocalStrategy(async (입력한아이디, 입력한비번, cb) => {
+    let result = await db.collection('user').findOne({ username: 입력한아이디 })
+    if (!result) {
+        return cb(null, false, { message: '아이디 DB에 없음' })
+    }
+    if (result.password == 입력한비번) {
+        return cb(null, result)
+    } else {
+        return cb(null, false, { message: '비번불일치' });
+    }
+}))
+
+passport.serializeUser((user, done) => {
+    console.log(user)
+    process.nextTick(() => {
+        done(null, { id: user._id, username: user.username })
+    })
+})
+
+passport.deserializeUser((user, done) => {
+    process.nextTick(() => {
+      return done(null, user)
+    })
+  })
+
+app.get('/login', (req, res)=>{
+    res.render('login.ejs')
+})
+
+app.post('/login', async (req, res, next) => {
+    passport.authenticate('local', (error, user, info) => {
+        if (error) return res.status(500).json(error)
+        if (!user) return res.status(401).json(info.message)
+        req.logIn(user, (err) => {
+            if (err) return next(err)
+            res.redirect('/')
+        })
+    })(req, res, next)
 })
